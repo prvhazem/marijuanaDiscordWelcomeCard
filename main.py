ade4d5b87bbab1b80fb6f59df842f98f46c76f7a
enghazemfouda <32505378-enghazemfouda@users.noreply.replit.com>
2025-02-27 10:46:41 +0000

    Fix: Correct uvicorn server startup command.
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/f31b90a2-f716-4923-a68b-5fa823b3989e/53822732-0fa8-4ceb-b965-0caf910027da.jpg

--- main.py
+++ main.py
@@ -77,4 +77,4 @@ async def generate_card(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
