37fa47e156f8ba1a4ca1b54155dd6d849dfcec57
Hazem Fouda <110072740+The-fouda@users.noreply.github.com>
2025-02-28 14:14:55 +0200

    card_generator.py

--- card_generator.py
+++ card_generator.py
@@ -4,7 +4,7 @@ import requests
 from io import BytesIO
 logging.basicConfig(level=logging.DEBUG)
 logger=logging.getLogger(__name__)
-BACKGROUND_URL="https://i.imgur.com/mPXCxjs.png"
+BACKGROUND_URL="https://files.catbox.moe/vr59iz.png"
 def download_image(url,default_size=(255,255)):
     try:
         response=requests.get(url,stream=True,timeout=5)
